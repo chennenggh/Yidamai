16d505f3c7cbf0d6c725c15cc516303cf92e180c
chennenggh <1934346652@qq.com>
2016-07-14 18:08:00 +0800

    新增一个类

---
+++ QQ/QQ/YiDa_VC.h
@@ -0,0 +1,13 @@
+//
+//  YiDa_VC.h
+//  QQ
+//
+//  Created by mac2015 on 16/7/14.
+//  Copyright © 2016年 能奎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YiDa_VC : UIViewController
+
+@end
